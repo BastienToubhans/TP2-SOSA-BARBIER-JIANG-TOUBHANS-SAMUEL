630baa8cce8543100ef0be8028b6d37275895c83
user <user@user-VirtualBox>
2016-11-24 09:51:53 +0100

    lib fstream

---
+++ TP2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 24 novembre 2016, 09:50
+ */
+
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
